6cfa9703f42d18a68fee642d41d92003da0b71ff
YanC Lin <yonalin1@gmail.com>
2018-09-21 19:54:16 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   private
-  
-    def logged_in?
+      def logged_in?
       !!session[:token]
     end
     
