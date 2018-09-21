56f043e6315bab7ea63fd168d082610233d0781b
YanC Lin <yonalin1@gmail.com>
2018-09-21 19:58:37 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   private
+  def authenticate_user
+  client_id = ENV['FOURSQUARE_CLIENT_ID']
+  redirect_uri = CGI.escape("http://localhost:3000/auth")
+  foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
+  redirect_to foursquare_url unless logged_in?
+end
       def logged_in?
       !!session[:token]
     end
